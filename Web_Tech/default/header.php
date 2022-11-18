ff79762322c4d66e685aa6a8b2f6d6f7e877b77e
kerbs <91007525+Kerbsby@users.noreply.github.com>
2022-11-18 12:29:59 +0800

    Add files via upload

---
+++ Web_Tech/default/header.php
@@ -0,0 +1,151 @@
+<?php
+
+require_once 'D:\xampp\htdocs\Web_Tech\config\config.php';
+
+
+session_start();
+error_reporting(0);
+
+
+if (isset($_POST['loginuser'])) {
+    $email = $_POST['email'];
+    $password = md5($_POST['password']);
+
+    $sql = "SELECT * FROM patient WHERE p_email = '$email' AND p_password='$password'";
+    $result = mysqli_query($conn, $sql);
+
+    if ($result->num_rows > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['email'] =$email;
+        header("Location: /Web_Tech/user/userhome.php");
+    }
+    elseif (isset($_POST['loginuser'])){
+        $email = $_POST['email'];
+        $password = md5($_POST['password']);
+
+        $sql = "SELECT * FROM doctor WHERE d_email = '$email' AND d_password='$password'";
+        $result = mysqli_query($conn, $sql);
+
+        if ($result->num_rows > 0) {
+            $row = mysqli_fetch_assoc($result);
+            $_SESSION['email'] =$email;
+            header("Location: /Web_Tech/doctor/doctorhome.php");
+        }
+
+    }
+    else {
+        echo 'Email or Password is Wrong!';
+    }
+}
+
+?>
+
+
+
+
+
+
+
+<link rel="stylesheet" type="text/css" href="/Web_Tech/css/style.css">
+<link rel="stylesheet" type="text/css" href="/Web_Tech/css/reglog.css">
+
+
+<body>
+
+<div class="top-nav" id="home">
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-auto">
+                <p>
+                    <i class="fa-regular fa-envelope"></i>
+                    <span>medbook@gmail.com</span>
+                </p>
+                <p>
+                    <i class="fa-solid fa-phone"></i>
+                    <span>991-2494</span>
+                </p>
+            </div>
+            <div class="col-auto social-icons">
+                <a href=""><i class="fa-brands fa-facebook-f"></i></a>
+                <a href=""><i class="fa-brands fa-twitter"></i></i></a>
+                <a href=""><i class="fa-brands fa-github"></i></i></a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<header class="sticky-top">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+        <div class="container">
+            <a href="#" class="navbar-brand mb-0" >
+                <i class="fa-solid fa-truck-medical"></i>MedBook
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-lable="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav" id="nav_left">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="/Web_Tech/index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ms-auto" id="nav_right">
+                    <li class="nav-item dropdown collapse navbar-collapse" id="navbarSupportedContent">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Signup
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item"href="/Web_Tech/user/register.php">User</a></li>
+                            <li><a class="dropdown-item" href="/Web_Tech/doctor/doctor_reg.php">Doctor</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown collapse navbar-collapse" id="navbarSupportedContent">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Login
+                        </a>
+                        <ul id="login-dp" class="dropdown-menu">
+                            <li>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <form action="" class="form" method="POST">
+                                                <div class="form-group">
+                                                    <label class="sr-only">Email</label><input type="email" class="form-control input-lg " placeholder="Email Address" id="email" name="email" required>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label class="sr-only">Password</label> <input type="password" class="form-control input-lg" id="password" placeholder="Password" name="password" required>
+                                                </div>
+                                                <a class="navbar-text" style="font-size: 14px;" href="/Web_Tech/doctor/doctorlogin.php">Are you a doctor? Click here</a>
+                                            <div class="form-group d-md-flex justify-content-md-end">
+                                                <button type="submit" class="btn btn-block" name="loginuser" id="loginuser">Login</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+
+
+
+
+</body>
+</html>
+
---
+++ appointment_db.sql
@@ -0,0 +1,143 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 18, 2022 at 05:28 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `appointment_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment`
+--
+
+CREATE TABLE `appointment` (
+  `requestID` int(11) NOT NULL,
+  `d_id` int(100) NOT NULL,
+  `p_email` varchar(100) NOT NULL,
+  `appDate` date NOT NULL,
+  `appTime` time NOT NULL,
+  `appSymptoms` text NOT NULL,
+  `appComments` text NOT NULL,
+  `status` varchar(10) NOT NULL DEFAULT 'process'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor`
+--
+
+CREATE TABLE `doctor` (
+  `doctorID` int(11) NOT NULL,
+  `d_FullName` varchar(100) NOT NULL,
+  `d_email` varchar(100) NOT NULL,
+  `d_number` int(12) NOT NULL,
+  `d_address` varchar(100) NOT NULL,
+  `d_password` varchar(100) NOT NULL,
+  `d_DOB` date NOT NULL,
+  `d_gender` varchar(100) NOT NULL,
+  `doctor_type` varchar(100) NOT NULL DEFAULT 'doctor',
+  `d_specialize` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `doctor`
+--
+
+INSERT INTO `doctor` (`doctorID`, `d_FullName`, `d_email`, `d_number`, `d_address`, `d_password`, `d_DOB`, `d_gender`, `doctor_type`, `d_specialize`) VALUES
+(2, 'Jacob Satorium', '123@gmail.com', 8687678, 'nkabdhja', 'b59c67bf196a4758191e42f76670ceba', '1994-09-15', 'male', 'doctor', 'Agricultural Specialist'),
+(3, 'adaadadadad', 'doc@gmail.com', 8687678, 'nkabdhja', 'b59c67bf196a4758191e42f76670ceba', '1996-09-17', 'female', 'doctor', 'adadawd'),
+(4, 'Zafina', 'doctor@gmail.com', 123123123, 'nkabdhja', 'b59c67bf196a4758191e42f76670ceba', '1996-10-14', 'female', 'doctor', 'awdadawa');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `patientID` int(11) NOT NULL,
+  `p_FullName` varchar(100) NOT NULL,
+  `p_password` varchar(100) NOT NULL,
+  `p_DOB` date NOT NULL,
+  `p_gender` varchar(20) NOT NULL,
+  `p_address` varchar(100) NOT NULL,
+  `p_number` int(12) NOT NULL,
+  `p_email` varchar(100) NOT NULL,
+  `patient_type` varchar(100) NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`patientID`, `p_FullName`, `p_password`, `p_DOB`, `p_gender`, `p_address`, `p_number`, `p_email`, `patient_type`) VALUES
+(89, 'Kervy Yu', 'b59c67bf196a4758191e42f76670ceba', '1998-11-17', 'male', 'Tugbungan', 2147483647, 'junjun@gmail.com', 'user'),
+(90, 'Nadyn Tadeos', 'b59c67bf196a4758191e42f76670ceba', '1996-09-16', 'female', 'kervy@gmail.com', 2147483647, 'kervy@gmail.com', 'user'),
+(92, 'Abra', '81dc9bdb52d04dc20036dbd8313ed055', '1995-10-16', 'male', 'Guiwan', 2147483647, 'kervy_yu@gmail.com', 'user');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`requestID`);
+
+--
+-- Indexes for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`doctorID`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`patientID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `requestID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
+
+--
+-- AUTO_INCREMENT for table `doctor`
+--
+ALTER TABLE `doctor`
+  MODIFY `doctorID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `patient`
+--
+ALTER TABLE `patient`
+  MODIFY `patientID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
