01871d04ea7f306c6441fdb1535ce7bff9a03ae1
kerbs <91007525+Kerbsby@users.noreply.github.com>
2022-11-24 02:06:37 +0800

    Add files via upload

---
+++ Web_Tech/index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/bf1c643ee2.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="Web_Tech/css/style.css">
+    <title>MedBook</title>
+</head>
+
+<body>
+
+<?php
+    include 'D:\xampp\htdocs\Web_Tech\default\header.php';
+?>
+
+<!--image-->
+
+<div class="slider-wrapper">
+
+
+    <div class="slide1 bg-cover d-flex align-items-center" style="height: 80vh;">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h6 class="text-uppercase text-white">Your illness, Our happiness!</h6>
+                    <h1 class="display-2 my-4 text-white text-uppercase">Certified Doctor's <br>Appointment System</h1>
+                    <p class="text-white">Please login to make an appointment</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<!--bottom content-->
+<section id="content-1-9" class="content-1-9 content-block">
+    <div class="container">
+        <div class="underlined-title mb-3 text-center">
+            <h1>The Future of Healing</h1>
+            <hr>
+            <h2>Why choose us?</h2>
+        </div>
+        <div class="row text-center mb-3" id="contents">
+            <div class="col-md-4 col-sm-12 col-xs-12 pad25 mb-5">
+                <div class="feats col-xs-2 border shadow">
+                    <span class="fa-solid fa-user-doctor"></span>
+                </div>
+                <div class="col-xs-10">
+                    <h4>Qualified Doctors</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-12 col-xs-12 pad25">
+                <div class="feats col-xs-2 border shadow">
+                    <span class="fa-solid fa-heart-pulse"></span>
+                </div>
+                <div class="col-xs-10">
+                    <h4>Emergency Care</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-12 col-xs-12 pad25">
+                <div class="feats col-xs-2 border shadow ">
+                    <span class="fa-solid fa-hand-holding-medical"></span>
+                </div>
+                <div class="col-xs-10">
+                    <h4>Great Service</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-12 col-xs-12 pad25">
+                <div class="feats col-xs-2 border shadow">
+                    <span class="fa-solid fa-shield-heart"></span>
+                </div>
+                <div class="col-xs-10">
+                    <h4>Quality and Safety</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-12 col-xs-12 pad25">
+                <div class="feats col-xs-2 border shadow">
+                    <span class="fa-regular fa-clipboard"></span>
+                </div>
+                <div class="col-xs-10">
+                    <h4>Personalized Treatments</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-12 col-xs-12 pad25">
+                <div class="feats col-xs-2 border shadow">
+                    <span class="fa-solid fa-microscope"></span>
+                </div>
+                <div class="col-xs-10">
+                    <h4>Advance Equipments</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
+</section>
+
+<!--footer-->
+<footer>
+    <div class="footer_top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 mx-auto">
+                        <a class="navbar-brand" href="">MedBook</a>
+                    <p>Lorem ipsum dolor sit amet. In dolor rerum eum soluta reiciendis et omnis velit. Et molestias neque qui sapiente ratione aut tempore soluta.</p>
+                <div class="socal-links">
+                    <a href=""><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href=""><i class="fa-brands fa-twitter"></i></i></a>
+                    <a href=""><i class="fa-brands fa-github"></i></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6 mx-auto mt-3">
+                    <div class="footer_widget">
+                        <h5 class="text-uppercase mb-4 font-weight-bold">Quick Links</h5>
+                            <p><a href="#" class="text-white">Home</a></p>
+                            <p><a href="#" class="text-white">About Us</a> </p>
+                             <p><a href="#" class="text-white">Appointment</a></p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <div class="footer_address">
+                        <h5 class="text-uppercase mb-4 font-weight-bold">Contact Us</h5>
+                            <p><i class="fa-solid fa-location-dot mr-3"></i>Zamboanga City</p>
+                            <p><i class="fa-solid fa-phone mr-3"></i>+63 9273930696</p>
+                            <p><i class="fa-solid fa-envelope mr-3"></i>abc_def@gmail.com</p>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer_bottom">
+        <div class="container">
+            <p class="mb-0">Lorem ipsum dolor sit amet. In dolor rerum eum soluta reiciendis </p>
+            <p class="pull-right small"><a href="/Web_Tech/admin/adminlogin.php" style="color: white; text-decoration: none;">admin</a></p>
+        </div>
+    </div>
+
+</footer>
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+
+</body>
+</html>
+
+
+
+
+
+
+
